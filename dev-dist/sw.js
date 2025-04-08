eb2a4cd3a18dd2345aebf56ecd7979cd00107d46
Eugene <eugene@omniguider.com>
2025-04-08 10:02:14 +0800

    [*] fix vulnerabilities.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "bb81156ad793bf9e2b9ee54332d5f858"
   }, {
     "url": "index.html",
-    "revision": "0.fh7c1phvc28"
+    "revision": "0.gu1gso0ckmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
